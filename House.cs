9b44df0dd6f1d672056eaf9154555a5b9c530545
tach9 <tach9@DESKTOP-1AEJVRA>
2023-07-30 22:23:15 -0600

    Add project files.

---
+++ House.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_1_1
+{
+    internal class House
+    {
+        public int HouseNumber { get; set; }
+        public string Address { get; set; }
+        public string HouseType { get; set; }
+        public House Next { get; set; }
+
+        public House(int houseNumber, string address, string houseType)
+        {
+            HouseNumber = houseNumber;
+            Address = address;
+            HouseType = houseType;
+            Next = null;
+        }
+    }
+
+    class HouseLinkedList
+    {
+        private House head;
+
+        public HouseLinkedList()
+        {
+            head = null;
+        }
+
+        public void AddHouse(int houseNumber, string address, string houseType)
+        {
+            House newHouse = new House(houseNumber, address, houseType);
+            if (head == null)
+            {
+                head = newHouse;
+            }
+            else
+            {
+                House current = head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+                current.Next = newHouse;
+            }
+        }
+
+        public House SearchHouse(int houseNumber)
+        {
+            House current = head;
+            while (current != null)
+            {
+                if (current.HouseNumber == houseNumber)
+                {
+                    return current;
+                }
+                current = current.Next;
+            }
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment6_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HouseLinkedList houseList = new HouseLinkedList();
+
+            houseList.AddHouse(1, "123 Main Street", "Colonial");
+            houseList.AddHouse(2, "456 Oak Avenue", "Ranch");
+            houseList.AddHouse(3, "789 Elm Road", "Cottage");
+
+            Console.Write("Enter the house number to search: ");
+            int searchHouseNumber = int.Parse(Console.ReadLine());
+
+            House foundHouse = houseList.SearchHouse(searchHouseNumber);
+            if (foundHouse != null)
+            {
+                Console.WriteLine("\nHouse Details:");
+                Console.WriteLine("House Number: " + foundHouse.HouseNumber);
+                Console.WriteLine("Address: " + foundHouse.Address);
+                Console.WriteLine("House Type: " + foundHouse.HouseType);
+            }
+            else
+            {
+                Console.WriteLine("\nHouse with number " + searchHouseNumber + " not found.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
